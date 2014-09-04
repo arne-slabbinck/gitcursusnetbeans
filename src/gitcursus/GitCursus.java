f0e3ed80eab2b1788be4ddc701c86429f09b4884
Arne Slabbinck <arne.slabbinck@gmail.com>
2014-09-04 13:06:13 +0200

    hallo

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package gitcursus;
 
 /**
@@ -17,6 +16,7 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hallo");
     }
-    
+
 }
